f25a8fae8249a42867dc2416fe8acfa0f32eb21c
dawood1408 <143161824+dawood1408@users.noreply.github.com>
2023-09-11 09:30:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+
+// Chapter : Alert
+// Q 1
+alert("Dawood")
+// Q 2
+alert("Khan")
+// Q 3
+alert("arslannoor.1408@gmail.com")
+// Q 04
+alert("03152107379")
+// Q 5
+alert("Passward")
+// Q 6
+alert("You are learning javascript")
+// Chapter Variables and String
+// Q 1
+// var FirstName = "Dawood"
+// var lastName = "khan"
+// var fullName ='firstName + lastName';
+// alert("fullName");
+var message = "This is your alert message.";
+alert(message)
+// Chapter : 02
+// Q:01
+var firstName = "Dawood"
+// Q :02
+var myVariable; // Declaration without definition
+myVariable = "Hello, World!"; // Assignment with a string
+alert(myVariable)
+// Q:03
+var teamName = "Pakistan"
+alert(teamName)
+// Q:04
+var bestMan = "Charlie"
+bestMan ="John"
+alert (bestMan)
+// chap:03
+// Q:01 
+var CaseQty = 144
+// Q:02
+var CaseQty = 144
+alert (CaseQty)
+// Q:03
+var sum ; 3 + 6
+sum = 9
+alert(sum)
+// Q:04
+var merchTotal =100;
+var shippingCharge =10;
+var orderTotal =merchTotal +shippingCharge
+alert (orderTotal)
+// Q:05
+var firstValue =100;
+var  secondValue =50;
+var totalValue = firstValue + secondValue
+alert(totalValue)
+// Chapter : 04
+// Q :01
+var productCost = 3.45;
+// Q:02
+var nameOfBand;
+// Q:03
+var firstName = Dawood
+var lastName = Khan
+var fullName =  firstName + lastName
+alert(fullName)
+// chap:05
+// Q:01
+var remainder = 10 % 3; 
+// Q :02
+var num = 20 % 6;
+num= 3.333;
+alert(num)
+// Q: 04
+var variable1 = 10;
+var variable2 = 5;
+var result = variable1 - variable2;
+// Q:06
+var num1 = 5;
+var num2 = 10;
+var result = num1 * num2;
+alert(result);
